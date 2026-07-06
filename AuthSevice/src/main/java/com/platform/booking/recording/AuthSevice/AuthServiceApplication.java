acab9c12062bc3a2a5542434aec8ecca93a4e6cc
Vasia Zakharchuk <vasiazakharchukk@gmail.com>
2026-07-06 16:52:55 +0300

    Added database configuration, added basic configurations

--- AuthSevice/src/main/java/com/platform/booking/recording/AuthSevice/AuthServiceApplication.java
+++ AuthSevice/src/main/java/com/platform/booking/recording/AuthSevice/AuthServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthSeviceApplication {
+public class AuthServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthSeviceApplication.class, args);
+		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
 }
---
+++ AuthSevice/src/main/java/com/platform/booking/recording/AuthSevice/config/KafkaConfig.java
@@ -0,0 +1,7 @@
+package com.platform.booking.recording.AuthSevice.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaConfig {
+}
