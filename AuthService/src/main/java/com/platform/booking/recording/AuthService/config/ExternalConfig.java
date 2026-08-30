874267d882628609f813b1eb8010704effc6c774
Vasia Zakharchuk <vasiazakharchukk@gmail.com>
2026-08-30 17:23:26 +0300

    Added configuration for config service. Moved all configuration to config server. Changed configuration in microservices to work with config server.

--- AuthService/src/main/java/com/platform/booking/recording/AuthService/config/ExternalConfig.java
+++ AuthService/src/main/java/com/platform/booking/recording/AuthService/config/ExternalConfig.java
@@ -2,6 +2,7 @@ package com.platform.booking.recording.AuthService.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
--- EmailService/src/main/java/com/platform/booking/recording/EmailService/config/ExternalConfig.java
+++ EmailService/src/main/java/com/platform/booking/recording/EmailService/config/ExternalConfig.java
@@ -2,11 +2,13 @@ package com.platform.booking.recording.EmailService.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix = "external")
 @Data
+@RefreshScope
 public class ExternalConfig {
 
     private Kafka kafka = new Kafka();
