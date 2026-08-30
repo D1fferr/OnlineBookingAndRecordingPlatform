3ea920a1396c5a234e17e60ebcb1b2ac5046563e
Vasia Zakharchuk <vasiazakharchukk@gmail.com>
2026-08-30 15:59:43 +0300

    added config server

---
+++ ConfigServer/src/main/java/com/platform/booking/recording/ConfigServer/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.platform.booking.recording.ConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
